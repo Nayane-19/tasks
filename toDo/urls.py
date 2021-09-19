0e5095638f6f18c8a13097b8a4d0851ecff7761a
Nayane-19 <nayane@Nayane-PC.localdomain>
2021-09-19 16:14:06 -0300

    add task

--- toDo/urls.py
+++ toDo/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('helloworld', views.helloWorld),
-    path('', views.todoList, name='todo-list')
+    path('', views.todoList, name='todo-list'),
+    path('toDo/<int:id>', views.todoView, name='todo-view'),
+    path('newtask/', views.newTask, name='new-task'),
 ]
--- toDo/views.py
+++ toDo/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import ToDoForm
+
+from .models import ToDo
 
 # Create your views here.
 
+def todoList(request):
+    todos = ToDo.objects.all().order_by('-created_at')
+    return render(request, 'tasks/list.html', {'todos': todos})
+
+def todoView(request, id):
+    todo = get_object_or_404(ToDo, pk=id)
+    return render(request, 'tasks/task.html', {'todo': todo})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = ToDoForm(request.POST)
+        
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.done = 'doing'
+            todo.user = request.user
+            todo.save()
+            return redirect('/')
+    else:
+        form = ToDoForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+
 def helloWorld(request):
     return HttpResponse('Hello World!')
-
-def todoList(request):
-    return render(request, 'tasks/list.html')
